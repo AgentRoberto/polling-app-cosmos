03b64857364e3bdac39eaefcbe8bb2be5fc1b787
Rob Sherwani <sherwanirobert@gmail.com>
2021-07-31 16:23:06 -0400

    Front-end chamges

--- vue/src/views/Types.vue
+++ vue/src/views/Types.vue
@@ -2,12 +2,22 @@
 	<div>
 		<div class="container">
 			<!-- this line is used by starport scaffolding # 4 -->
+			<h3>
+				Voter Module
+			</h3>
+			<PollForm />
+			<PollList />
 		</div>
 	</div>
 </template>
 
 <script>
+import PollForm from "../components/PollForm";
+import PollList from "../components/PollList";
+
 export default {
-	name: 'Types'
+  name: 'Types',
+  components: { PollForm, PollList },
 }
+
 </script>
